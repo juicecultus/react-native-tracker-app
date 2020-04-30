cf0c02949b7908405c062f81f3d4be03a2edfd8b
juicecultus <juicecultus@gmail.com>
2020-04-30 20:45:03 +0100

    hookup screen navigation w stack and tab

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/SignupScreen';
@@ -11,11 +12,26 @@ import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function GroupHome() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="TrackCreate" component={TrackCreateScreen} />
+      <Tab.Screen name="Account" component={AccountScreen} />
+      <Stack.Navigator>
+        <Stack.Screen name="TrackList" component={TrackListScreen} />
+        <Stack.Screen name="TrackDetail" component={TrackDetailScreen} />
+      </Stack.Navigator>
+    </Tab.Navigator>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="Home" component={GroupHome} />
         <Stack.Screen name="SignIn" component={SigninScreen} />
         <Stack.Screen name="SignUp" component={SignupScreen} />
       </Stack.Navigator>
