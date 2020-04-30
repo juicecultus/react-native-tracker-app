7cdd69f8497a5b32e5b0e1dbd591f6e536777788
juicecultus <juicecultus@gmail.com>
2020-05-01 00:06:58 +0100

    evaluate context requirements for app

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import {Input} from 'react-native-elements';
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -7,8 +8,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = dispatch => {
+  return ({email, password}) => {
+    // make api request to sign up with email and password
+    // if signed up, modify state to say we are authenticated
+    // if fail, convey error message to user
+  };
+};
+
+const signin = dispatch => {
+  return ({email, password}) => {
+    // try to sign in
+    // handle success by undating state
+    // if fail, convey error message to user
+  };
+};
+
+const signout = dispatch => {
+  return ({email, password}) => {
+    // sign out
+  };
+};
+
 export const {Provider, Context} = createDataContext(
   authReducer,
-  {},
+  {signup, signin, signout},
   {isSignedIn: false},
 );
