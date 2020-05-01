4c466d13a3a0808649bfb92e3269726b6fd4b1af
juicecultus <juicecultus@gmail.com>
2020-05-01 17:42:43 +0100

    wire up signin screen

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,26 +5,10 @@ import * as RootNavigation from '../RootNavigation';
 
 const authReducer = (state, action) => {
   switch (action.type) {
-    case 'signup':
-      return {errorMessage: '', userToken: action.payload};
-    case 'restore_token':
-      return {
-        ...state,
-        userToken: action.payload,
-        isLoading: false,
-      };
-    case 'sign_in':
-      return {
-        ...state,
-        isSignout: false,
-        userToken: action.payload,
-      };
-    case 'sign_out':
-      return {
-        ...state,
-        isSignout: true,
-        userToken: null,
-      };
+    case 'signin':
+      return {errorMessage: '', token: action.payload};
+    case 'signout':
+      return {...state, token: null};
     case 'add_error':
       return {...state, errorMessage: action.payload};
     default:
@@ -35,8 +19,8 @@ const authReducer = (state, action) => {
 const signup = dispatch => async ({email, password}) => {
   try {
     const response = await trackerApi.post('/signup', {email, password});
-    await AsyncStorage.setItem('userToken', response.data.token);
-    dispatch({type: 'signup', payload: response.data.token});
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({type: 'signin', payload: response.data.token});
     RootNavigation.navigate('Home');
   } catch (err) {
     dispatch({
@@ -46,36 +30,39 @@ const signup = dispatch => async ({email, password}) => {
   }
 };
 
-const bootstrapAsync = dispatch => async () => {
-  let userToken;
+// const bootstrapAsync = dispatch => async () => {
+//   let userToken;
+//   try {
+//     userToken = await AsyncStorage.getItem('userToken');
+//     dispatch({type: 'restore_token', payload: userToken});
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
+
+const signin = dispatch => async ({email, password}) => {
   try {
-    userToken = await AsyncStorage.getItem('userToken');
-    dispatch({type: 'restore_token', payload: userToken});
-  } catch (err) {
-    console.log(err.message);
+    const response = await trackerApi.post('/signin', {email, password});
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({type: 'signin', payload: response.data.token});
+    RootNavigation.navigate('Home');
+  } catch (error) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with sign in',
+    });
   }
 };
 
-const signin = dispatch => {
-  return ({email, password}) => {
-    // try to sign in
-    // handle success by undating state
-    // if fail, convey error message to user
-  };
-};
-
 const signout = dispatch => {
-  return () => dispatch({type: 'sign_out'});
+  // return () => dispatch({type: 'sign_out'});
 };
 
 export const {Provider, Context} = createDataContext(
   authReducer,
-  {signup, signin, signout, bootstrapAsync},
+  {signup, signin},
   {
     token: null,
     errorMessage: '',
-    userToken: null,
-    isSignout: false,
-    isLoading: true,
   },
 );
