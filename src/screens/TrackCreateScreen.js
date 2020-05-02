fe74454613090d08cae6eed39fb6647ec38c52b0
juicecultus <juicecultus@gmail.com>
2020-05-02 12:18:57 +0100

    read location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,7 +3,11 @@ import React, {useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {StyleSheet} from 'react-native';
 import {Text} from 'react-native-elements';
-import {requestPermissionsAsync} from 'expo-location';
+import {
+  requestPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from 'expo-location';
 
 import Map from '../components/Map';
 import Spacer from '../components/Spacer';
@@ -14,6 +18,16 @@ const TrackCreateScreen = () => {
   const startWatching = async () => {
     try {
       await requestPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        location => {
+          console.log(location);
+        },
+      );
     } catch (e) {
       seterr(e);
     }
