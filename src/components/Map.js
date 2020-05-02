9c63ac43a5299288f2acd597aa33e351c1a26908
juicecultus <juicecultus@gmail.com>
2020-05-02 18:07:53 +0100

    draw position indicator

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import MapView, {Polyline} from 'react-native-maps';
+import MapView, {Polyline, Circle} from 'react-native-maps';
 import {StyleSheet, ActivityIndicator} from 'react-native';
 import {Context as LocationContext} from '../context/LocationContext';
 
@@ -20,12 +20,20 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-      region={{
-        ...currentLocation.coords,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }}
-    />
+      // add region if you want map to move with the tracker
+      // region={{
+      //   ...currentLocation.coords,
+      //   latitudeDelta: 0.01,
+      //   longitudeDelta: 0.01,
+      // }}
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
+    </MapView>
   );
 };
 
