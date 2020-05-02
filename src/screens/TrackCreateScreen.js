a29785dc4db9e5ab9ef184f6c04ed22db780b8b9
juicecultus <juicecultus@gmail.com>
2020-05-02 11:20:54 +0100

    request location permission

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {StyleSheet} from 'react-native';
 import {Text} from 'react-native-elements';
+import {requestPermissionsAsync} from 'expo-location';
 
 import Map from '../components/Map';
+import Spacer from '../components/Spacer';
 
 const TrackCreateScreen = () => {
+  const [err, seterr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      seterr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text h3>Create a Track</Text>
+      <Text h3 style={styles.header}>
+        Create a Track
+      </Text>
+      <Spacer />
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 };
 
 export default TrackCreateScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    textAlign: 'center',
+  },
+});
